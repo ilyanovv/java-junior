2b470f1105871c9e08d0bb890850b6d45c56516b
ilyanovv <ilyanovv@gmail.com>
2017-09-27 16:52:04 +0300

    removed code dublications

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,28 +1,37 @@
 package com.acme.edu;
 
+
+import java.io.PrintStream;
+
 public class Logger {
+    private final static String PRIMITIVE_PREFIX = "primitive: ";
+    private final static String REFERENCE_PREFIX = "reference: ";
+    private final static String CHAR_PREFIX = "char: ";
+    private final static String STRING_PREFIX = "string: ";
+    private static PrintStream printStream = System.out;
+
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        System.out.println(PRIMITIVE_PREFIX + message);
     }
 
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        printStream.println(PRIMITIVE_PREFIX + message);
     }
 
     public static void log(char message){
-        System.out.println("char: "+ message);
+        printStream.println(CHAR_PREFIX + message);
     }
 
     public static void log(String message){
-        System.out.println("string: " + message);
+        printStream.println(STRING_PREFIX + message);
     }
 
     public static void log(boolean message) {
-        System.out.println("primitive: " + message);
+        printStream.println(PRIMITIVE_PREFIX + message);
     }
 
     public static void log(Object message) {
-        System.out.println("reference: " + message.toString());
+        printStream.println(REFERENCE_PREFIX + message.toString());
     }
 
     public static void main(String[] args) {
